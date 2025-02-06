b402ea09733bc7d90add9579bdf063acf3ac147a
menglingqi <menglingqi@pansoft.com>
2025-02-06 23:52:06 +0800

    定义全局异常处理器和跨域配置类

---
+++ src/main/java/com/meng/mengpicturebackend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.meng.mengpicturebackend.exception;
+
+import com.meng.mengpicturebackend.common.BaseResponse;
+import com.meng.mengpicturebackend.common.ResultUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * @DESCRIPTION: 全局异常处理器
+ * @AUTHOR: MENGLINGQI
+ * @TIME: 2025/2/6 23:41
+ **/
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public BaseResponse<?> businessExceptionHandler(BusinessException e) {
+        log.error("BusinessException", e);
+        return ResultUtils.error(e.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public BaseResponse<?> runtimeExceptionHandler(RuntimeException e) {
+        log.error("RuntimeException", e);
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, "系统错误");
+    }
+}
+
+
