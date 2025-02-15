d3f3ad44d249f313d7ff06ac82bc90af5331dae8
menglingqi <menglingqi@pansoft.com>
2025-02-16 00:07:52 +0800

    对外提供用户操作相关接口

--- src/main/java/com/meng/mengpicturebackend/controller/UserController.java
+++ src/main/java/com/meng/mengpicturebackend/controller/UserController.java
@@ -1,20 +1,27 @@
 package com.meng.mengpicturebackend.controller;
 
+import cn.hutool.core.bean.BeanUtil;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.meng.mengpicturebackend.annotation.AuthCheck;
 import com.meng.mengpicturebackend.common.BaseResponse;
+import com.meng.mengpicturebackend.common.DeleteRequest;
 import com.meng.mengpicturebackend.common.ResultUtils;
+import com.meng.mengpicturebackend.constant.UserConstant;
+import com.meng.mengpicturebackend.exception.BusinessException;
 import com.meng.mengpicturebackend.exception.ErrorCode;
 import com.meng.mengpicturebackend.exception.ThrowUtils;
-import com.meng.mengpicturebackend.model.dto.UserLoginRequest;
-import com.meng.mengpicturebackend.model.dto.UserRegisterRequest;
+import com.meng.mengpicturebackend.model.dto.user.*;
+import com.meng.mengpicturebackend.model.entity.User;
+import com.meng.mengpicturebackend.model.enums.UserRoleEnum;
 import com.meng.mengpicturebackend.model.vo.LoginUserVO;
+import com.meng.mengpicturebackend.model.vo.UserVO;
 import com.meng.mengpicturebackend.service.UserService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.BeanUtils;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /**
  * @DESCRIPTION: 用户controller
@@ -48,7 +55,7 @@ public class UserController {
      * 用户登录
      * @return
      */
-    @PostMapping("/register")
+    @PostMapping("/login")
     public BaseResponse<LoginUserVO> userLogin(@RequestBody UserLoginRequest userloginRequest, HttpServletRequest request){
         ThrowUtils.throwIf(userloginRequest == null, ErrorCode.PARAMS_ERROR);
         String userAccount = userloginRequest.getUserAccount();
@@ -56,6 +63,121 @@ public class UserController {
         LoginUserVO userRegister = userService.userLogin(userAccount, userPassword, request);
         return ResultUtils.success(userRegister);
     }
+
+    /**
+     * 获取当前登录用户
+     * @return
+     */
+    @GetMapping("/get/loginUser")
+    public BaseResponse<LoginUserVO> userLogin(HttpServletRequest request){
+        User loginUser = userService.getLoginUser(request);
+        return ResultUtils.success(userService.getLoginUserVO(loginUser));
+    }
+
+    /**
+     * 用户注销
+     * @return
+     */
+    @PostMapping("/logout")
+    public BaseResponse<Boolean> userLogout(HttpServletRequest request){
+        userService.userLoginOut(request);
+        return ResultUtils.success(true);
+    }
+
+    /**
+     * 创建用户
+     * @return
+     */
+    @PostMapping("/add")
+    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
+    public BaseResponse<Long> addUser(@RequestBody UserAddRequest userAddRequest){
+        ThrowUtils.throwIf(userAddRequest == null, ErrorCode.PARAMS_ERROR);
+        User user = new User();
+        BeanUtil.copyProperties(userAddRequest, user);
+        // 填充默认值
+        user.setUserPassword(userService.getEncryptPassword(UserConstant.USER_DEFAULT_PASSWORD));
+        boolean isAdd = userService.save(user);
+        ThrowUtils.throwIf(!isAdd, ErrorCode.OPERATION_ERROR);
+        return ResultUtils.success(user.getId());
+
+    }
+
+    /**
+     * 根据 id 获取用户（仅管理员）
+     */
+    @GetMapping("/get")
+    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
+    public BaseResponse<User> getUserById(long id) {
+        ThrowUtils.throwIf(id <= 0, ErrorCode.PARAMS_ERROR);
+        User user = userService.getById(id);
+        ThrowUtils.throwIf(user == null, ErrorCode.NOT_FOUND_ERROR);
+        return ResultUtils.success(user);
+    }
+
+    /**
+     * 根据 id 获取包装类
+     */
+    @GetMapping("/get/vo")
+    public BaseResponse<UserVO> getUserVOById(long id) {
+        BaseResponse<User> response = getUserById(id);
+        User user = response.getData();
+        return ResultUtils.success(userService.getUserVO(user));
+    }
+
+    /**
+     * 删除用户
+     */
+    @PostMapping("/delete")
+    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
+    public BaseResponse<Boolean> deleteUser(@RequestBody DeleteRequest deleteRequest) {
+        if (deleteRequest == null || deleteRequest.getId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        boolean b = userService.removeById(deleteRequest.getId());
+        return ResultUtils.success(b);
+    }
+
+    /**
+     * 更新用户
+     */
+    @PostMapping("/update")
+    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
+    public BaseResponse<Boolean> updateUser(@RequestBody UserUpdateRequest userUpdateRequest) {
+        if (userUpdateRequest == null || userUpdateRequest.getId() == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        User user = new User();
+        BeanUtils.copyProperties(userUpdateRequest, user);
+        boolean result = userService.updateById(user);
+        ThrowUtils.throwIf(!result, ErrorCode.OPERATION_ERROR);
+        return ResultUtils.success(true);
+    }
+
+    /**
+     * 分页获取用户封装列表（仅管理员）
+     *
+     * @param userQueryRequest 查询请求参数
+     */
+    @PostMapping("/list/page/vo")
+    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
+    public BaseResponse<Page<UserVO>> listUserVOByPage(@RequestBody UserQueryRequest userQueryRequest) {
+        ThrowUtils.throwIf(userQueryRequest == null, ErrorCode.PARAMS_ERROR);
+        // 提取当前页码（current）和每页大小（pageSize）
+        long current = userQueryRequest.getCurrent();
+        long pageSize = userQueryRequest.getPageSize();
+        // 分页查询
+        Page<User> userPage = userService.page(new Page<>(current, pageSize), userService.getQueryWrapper(userQueryRequest));
+        // 从查询出的User列表提取UserVO列表
+        List<UserVO> userVOList = userService.getUserVOList(userPage.getRecords());
+        // 新建一个Page对象，设置当前页码、每页大小和总记录数
+        Page<UserVO> userVOPage = new Page<>(current, pageSize, userPage.getTotal());
+        // 将UserVO列表设置到Page对象中
+        userVOPage.setRecords(userVOList);
+
+        return ResultUtils.success(userVOPage);
+    }
+
+
 
 
 }
